135e36c102579208978b4f7d53aed3fea89ee91c
Matt Rothenberg <mattrothenberg@gmail.com>
2025-01-15 15:10:34 -0500

    basic Hono API in front of all-the-public-replicate-models

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// @ts-ignore
+import stats from "all-the-public-replicate-models/stats";
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+
+const app = new Hono();
+
+app.get("/stats/:owner/:model", (c) => {
+  const { model, owner } = c.req.param();
+  if (!model || !owner) return c.text("Not found", 404);
+
+  const nwo = `${owner}/${model}`;
+  const match = stats[nwo];
+
+  if (!match) return c.text("Not found", 404);
+
+  return c.json(match || {}, 200);
+});
+
+const port = 3000;
+
+console.log(`Server is running on http://localhost:${port}`);
+
+serve({
+  fetch: app.fetch,
+  port,
+});
